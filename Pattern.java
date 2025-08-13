83bb492bf4d564f2fca9c41ff5a0a38bc8c33991
mdmoinshaikh <mdmoishaikh2143@gmail.com>
2025-08-13 10:51:06 +0530

    pattern 2 of isoceles triangle is created

--- Pattern.java
+++ Pattern.java
@@ -1,17 +1,14 @@
 
- class Pattern
-{
+ class Pattern 1 {
 public static void main(String args[])
-  {  
-    int n=5;
-    for (int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=i;j++)
-        {
-            System.out.print("*");
-        }
-          System.out.println();
+{int n=5;
+for (int i=1;i<=n;i++)
+{
+for(int j=1;j<=n-i;j++){
+System.out.print(" ");}
+for(int k=1;k<=i;k++){
+System.out.print("* ");}System.out.println();
+}
 
-    }
-  
 }}
+
